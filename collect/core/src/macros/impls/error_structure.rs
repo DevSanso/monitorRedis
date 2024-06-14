67a56d366cbf138966c79188c8fa4d90e2ed0d50
Sunken Ahn <devsanso@outlook.com>
2024-06-15 01:43:08 +0900

    [ #5 ] :  에러 구조체 생성 매크로 변경

--- collect/core/src/macros/impls/error_structure.rs
+++ collect/core/src/macros/impls/error_structure.rs
@@ -1,6 +1,5 @@
-#[macro_export]
-macro_rules! impl_error_structure {
-    ($name : ident, $descr : expr) => {
+macro_rules! impl_error {
+    ($name : ident, $message:expr, $descr : expr) => {
         
         #[derive(Debug)]
         pub struct $name;
@@ -26,38 +25,23 @@ macro_rules! impl_error_structure {
         }
 
     };
-
-    (arg, $name : ident, $descr : expr) => {
-        
-        #[derive(Debug)]
-        pub struct $name(pub String);
-
-        impl $name {
-            pub fn new(msg : String) -> Self {
-                $name(msg)
-            }
-        }
-
-        impl Display for $name {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                write!(f, "{}({})", stringify!($name), self.0)
-            }
-        }
-
-        impl Error for $name  {
-            fn source(&self) -> Option<&(dyn Error + 'static)> {
-                None
-            }
-        
-            fn description(&self) -> &str {
-                $descr
-            }
-        
-            fn cause(&self) -> Option<&dyn Error> {
-                self.source()
-            }
-        }
-
-    };
 }
+
+#[macro_export]
+macro_rules! impl_err_mod {
+    ($name:ident, [$((
+        $err_name:ident, $message:expr, $descr:expr)),*
+    ]) => {
+        pub mod $name {
+            use std::error::Error;
+            use std::fmt::Display;
+            use std::fmt::Debug;
+
+            $(impl_error!($err_name, $message, $descr);)*
+
+        }
+    }
+}
+
+
 
