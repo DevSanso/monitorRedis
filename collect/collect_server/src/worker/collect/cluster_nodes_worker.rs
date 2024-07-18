42825e90a11157bf39dc9ebf427acb12d0a48b25
Sunken Ahn <devsanso@outlook.com>
2024-07-18 23:26:15 +0900

    memory usage 커매드 추가, 기타 수정

--- collect/collect_server/src/worker/collect/cluster_nodes_worker.rs
+++ collect/collect_server/src/worker/collect/cluster_nodes_worker.rs
@@ -27,13 +27,12 @@ pub fn cluster_nodes_worker(link_key : i32, redis_conn : &'_ mut dbs::redis_pool
     let result = redis_conn.run_command(cmd, &[])?;
     let nodes = parsing_cluster_nodes(result)?;
     
-
-    let mut trans = pg_conn.trans()?;
-
     let sync_query = dbs_cmd::PG_COMMANDLINE_MAP.get(&dbs_cmd::PgCommand::SyncClusterNodes).unwrap();
     let push_query = dbs_cmd::PG_COMMANDLINE_MAP.get(&dbs_cmd::PgCommand::InsertClusterNodesPing).unwrap();
 
     for node in nodes {
+        let mut trans = pg_conn.trans()?;
+
         let sync_ret = trans.execute(&sync_query, &[&link_key, &node.node_id.as_str(), &node.ip.as_str(),
             &node.port, &node.cport, &node.node_type.as_str(), &node.master_node, &node.ping_epoch, &node.connect_type.as_str(), &slot_pg_bind_value(node.slots.as_slice())]);
 
@@ -48,9 +47,9 @@ pub fn cluster_nodes_worker(link_key : i32, redis_conn : &'_ mut dbs::redis_pool
             let _ = trans.rollback();
             return utils_inherit_error!(connection, CommandRunError, "push", push_ret.err().unwrap());
         }
-    }
 
-    let _ = trans.commit();
+        let _ = trans.commit();
+    }
 
     Ok(())
 }
